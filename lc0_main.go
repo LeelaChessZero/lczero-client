39316fe080a9b56bb586e0765b81218663dd9a9e
borg323 <borg323@users.noreply.github.com>
2018-06-16 23:25:51 +0300

    change help text with working --lc0args example

--- lc0_main.go
+++ lc0_main.go
@@ -37,7 +37,7 @@ var (
 	password = flag.String("password", "", "Password")
 //	gpu      = flag.Int("gpu", -1, "ID of the OpenCL device to use (-1 for default, or no GPU)")
 	debug    = flag.Bool("debug", false, "Enable debug mode to see verbose output and save logs")
-	lc0Args  = flag.String("lc0args", "", `Extra args to pass to the backend.  example: --lc0args="--parallelism=10 --threads=2"`)
+	lc0Args  = flag.String("lc0args", "", `Extra args to pass to the backend. Example: --lc0args=--backend=cudnn`)
 )
 
 // Settings holds username and password.
