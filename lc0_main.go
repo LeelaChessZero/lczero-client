3fefef28e543940d4ab13542f3bdfe05c22be62d
borg323 <39573933+borg323@users.noreply.github.com>
2019-02-08 00:12:49 +0200

    Bump version to 22

--- lc0_main.go
+++ lc0_main.go
@@ -111,7 +111,7 @@ func getExtraParams() map[string]string {
 	return map[string]string{
 		"user":       *user,
 		"password":   *password,
-		"version":    "21",
+		"version":    "22",
 		"token":      strconv.Itoa(randId),
 		"train_only": strconv.FormatBool(*trainOnly),
 	}
