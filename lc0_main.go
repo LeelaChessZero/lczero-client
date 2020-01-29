ae9d2f7758bb9fff139b26055aaf537a7b806202
Tilps <Tilps@users.noreply.github.com>
2020-01-29 20:01:02 +1100

    Bump version post release

--- lc0_main.go
+++ lc0_main.go
@@ -119,7 +119,7 @@ func getExtraParams() map[string]string {
 	return map[string]string{
 		"user":       *user,
 		"password":   *password,
-		"version":    "24",
+		"version":    "25",
 		"token":      strconv.Itoa(randId),
 		"train_only": strconv.FormatBool(*trainOnly),
 		"hostname":   localHost,
