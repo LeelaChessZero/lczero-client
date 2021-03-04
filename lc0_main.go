0946a61819f88ea0b8029badfdaf8f6db7428eaf
borg323 <39573933+borg323@users.noreply.github.com>
2021-03-04 02:34:13 +0200

    Bump version post branch

--- lc0_main.go
+++ lc0_main.go
@@ -135,7 +135,7 @@ func getExtraParams() map[string]string {
 	return map[string]string{
 		"user":       *user,
 		"password":   *password,
-		"version":    "31",
+		"version":    "32",
 		"token":      strconv.Itoa(randId),
 		"train_only": strconv.FormatBool(*trainOnly),
 		"hostname":   *localHost,
